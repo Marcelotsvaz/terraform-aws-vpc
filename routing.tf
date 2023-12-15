4c2f4c0afc8465d3b28edb3265144580dab637ea
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2023-12-15 09:25:40 -0300

    Fix route to Internet Gateway

--- routing.tf
+++ routing.tf
@@ -4,6 +4,16 @@
 resource aws_default_route_table public {
 	default_route_table_id = aws_vpc.main.default_route_table_id
 	
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.main.id
+	}
+	
+	route {
+		ipv6_cidr_block = "::/0"
+		gateway_id = aws_internet_gateway.main.id
+	}
+	
 	tags = {
 		Name = "${var.name} Public Route Table"
 	}
@@ -19,12 +29,6 @@ resource aws_route_table_association public {
 	
 	route_table_id = aws_default_route_table.public.id
 	subnet_id = each.key
-}
-
-
-resource aws_route_table_association internet_gateway {
-	route_table_id = aws_default_route_table.public.id
-	gateway_id = aws_internet_gateway.main.id
 }
 
 
